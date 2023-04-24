76424a95ba81e7ac4649fc370db571708dd5f44e
Kusum Gandham <koolkusum10@gmail.com>
2023-04-23 22:06:53 -0400

    i go to sleep now

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import authReducer from "./state";
+import { configureStore} from "@reduxjs/toolkit";
+import {Provider} from "react-redux";
+import{
+  persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER
+}from "redux-persist";//can save in local state (usch as tab closed info will be gone unless logged out)
+import storage from "redux-persist/lib/storage";
+import {PersistGate} from "redux-persist/integration/react";
 
+const persistConfig = {key: "root", storage, version: 1};
+const persistedReducer = persistReducer (persistConfig, authReducer);
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck:{
+        ignoredActions : [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      },
+    }),
+
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store = {store}>    
     <App />
+    </Provider>
+
   </React.StrictMode>
 );
