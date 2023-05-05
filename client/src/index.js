4a35df933c08104b4654438941a1d4d4bf2d6c39
Kusum Gandham <koolkusum10@gmail.com>
2023-05-05 15:45:43 -0400

    colors and index

--- client/src/index.js
+++ client/src/index.js
@@ -15,7 +15,7 @@ const persistConfig = {key: "root", storage, version: 1};
 const persistedReducer = persistReducer (persistConfig, authReducer);
 const store = configureStore({
     reducer: persistedReducer,
-    middleware (getDefaultMiddleware) =>
+    middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck:{
         ignoredActions : [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
@@ -27,8 +27,10 @@ const store = configureStore({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store = {store}>    
+    <Provider store = {store}> 
+    <PersistGate loading = {null} persistor = {persistStore(store)}> 
     <App />
+    </PersistGate>  
     </Provider>
 
   </React.StrictMode>
